e48186d7a164350391b094d387849e8036c4ccc3
robertoalvesneto <robertoalvesnetoprodut@gmail.com>
2021-06-28 19:26:20 -0400

    test suite

--- tests/gc_textfield_test.py
+++ tests/gc_textfield_test.py
@@ -2,9 +2,9 @@ import unittest
 from googleforms.bot_forms import BotForms
 
 
-class TextFieldTest(unittest.TestCase):
-    website_url = "https://forms.gle/rUd5fBs5CPJv5ML29"
-    name_title_field = "Resposta curta"
+class TestTextField(unittest.TestCase):
+    website_url = "https://forms.gle/wtwFScuirGPmkxmK6"
+    name_title_field = "normal"
     insert_text = "texto aleatorio a ser inserido"
     path_base_root = "freebirdFormviewerComponentsQuestionBaseRoot"
     bf = None
@@ -49,5 +49,59 @@ class TextFieldTest(unittest.TestCase):
         self.assertIn(self.insert_text, input_field.get_attribute("value"))
 
 
+class SuiteTestsTextField():
+
+    def __set_botform_values(self, name_title_field):
+        insert_text = "texto aleatorio a ser inserido"
+        TestTextField.name_title_field = name_title_field
+        TestTextField.insert_text = insert_text
+
+    def __create_suite(self):
+        suite = unittest.TestSuite()
+        suite.addTest(TestTextField("test_get_element_by_title"))
+        suite.addTest(TestTextField("test_get_root_ancestral"))
+        suite.addTest(TestTextField("test_filled_field"))
+
+        return suite
+
+    def __builder_suite_test(self, name_title_field):
+        self.__set_botform_values(name_title_field)
+
+        return self.__create_suite()
+
+    def test_simple_text_field(self):
+        return self.__builder_suite_test("normal")
+
+    def test_required_text_field(self):
+        return self.__builder_suite_test("campo obrigatório")
+
+    def test_text_field_with_description(self):
+        return self.__builder_suite_test("com descrição")
+
+    def test_text_field_with_validate(self):
+        return self.__builder_suite_test("com descrição")
+
+    def test_text_field_with_image(self):
+        return self.__builder_suite_test("com imagem")
+
+    def test_text_fields_with_same_name(self):
+        return self.__builder_suite_test("teste campo com mesmo nome")
+
+    def test_text_fields_without_name(self):
+        return self.__builder_suite_test("")
+
+
 if __name__ == "__main__":
-    unittest.main()
+    # rodar os testes:
+    # python3 -m tests.gc_textfield_test
+    suite = SuiteTestsTextField()
+
+    runner = unittest.TextTestRunner()
+    runner.run(suite.test_simple_text_field())
+    runner.run(suite.test_required_text_field())
+    runner.run(suite.test_text_field_with_description())
+    runner.run(suite.test_text_field_with_validate())
+    runner.run(suite.test_text_field_with_image())
+    runner.run(suite.test_text_fields_with_same_name())
+    runner.run(suite.test_text_fields_without_name())
+    # unittest.main()
